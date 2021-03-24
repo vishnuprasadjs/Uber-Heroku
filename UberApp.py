3d38d1082c90898e404e080a2495261bd0ea994d
vishnuprasadjs21 <vishnuprasadjs21@gmail.com>
2021-03-24 21:22:35 +0530

    coordinate edit

--- UberApp.py
+++ UberApp.py
@@ -92,14 +92,19 @@ def get_route(source, destination, date,departure_time,holiday):
     folium_static(m)
 
 
+
+
 def run():
     date = st.sidebar.date_input('Select date', datetime.date(2020,1,1))
     time = st.sidebar.time_input('Select time', datetime.time(0,00))
     st.write('Date of Journey:',date)
     st.write('Time:',time)
-    source = st.sidebar.selectbox('Choose the source',coordinate_list)
+    points=['Select','Point 1','Point 2','Point 3','Point 4', 'Point 5', 'Point 6', 'Point 7','Point 8','Point 9','Point 10' ]
+    s_point = st.sidebar.selectbox('Choose the source',points)
+    source = coordinate_list.get(s_point)
     st.write('You selected source:', source)
-    destination = st.sidebar.selectbox('Choose the destination',coordinate_list)
+    d_point = st.sidebar.selectbox('Choose the destination',points)
+    destination=coordinate_list.get(d_point)
     st.write('You selected destination:', destination)
     
     url='https://www.google.com/maps/dir/{},{}/{},{}'.format(source[0],source[1],destination[0],destination[1])
@@ -137,16 +142,18 @@ if __name__ == "__main__":
     city=st.sidebar.selectbox('Choose the City',city_list)
     st.write('City: ',city)
     if city=='Bangalore':
-        coordinate_list=['Select',(12.946538, 77.579975),(13.04438892,77.60185844),(12.95275348,77.72982887),(12.88962743,77.63661686),
-                         (12.95067179,77.75920658),(12.95275348,77.72982887),(12.88243717,77.54024708),(12.8753325,77.54171853),
-                         (13.07513833,77.64474206),(13.01596484,77.6713507),(12.87707686,77.512009),(12.89076576,77.50186805),
-                         (13.10954031,77.61893643),(12.99447522,77.60787194),(13.01571239,77.6809775),(12.87034121,77.63979306)]
+        coordinate_list={'Select':'Select','Point 1':(12.946538, 77.579975),'Point 2':(13.04438892,77.60185844),'Point 3':(12.95275348,77.72982887),'Point 4':(12.88962743,77.63661686),
+                         'Point 5':(12.95067179,77.75920658),'Point 6':(12.95275348,77.72982887),'Point 7':(12.88243717,77.54024708),'Point 8':(12.8753325,77.54171853),
+                         'Point 9':(13.07513833,77.64474206),'Point 10':(13.01596484,77.6713507)}
+                         #,(12.87707686,77.512009),(12.89076576,77.50186805),
+                         #(13.10954031,77.61893643),(12.99447522,77.60787194),(13.01571239,77.6809775),(12.87034121,77.63979306)]
         pkl_filename = "Est_time_pred.pkl"
     if city=='Hyderabad':
-        coordinate_list=['select', (17.4990737222447,78.5484425537565),(17.3228369187765,78.4003341997112),(17.4289313805497,78.3074137286198),
-                         (17.5338767879618,78.4312953607305),(17.4501231060621,78.3179062429442),(17.5237690238243,78.5091847750287),
-                         (17.4894454531734,78.4112496777986),(17.4308989846564,78.5238677530526),(17.3011570696757,78.4250332621834),
-                         (17.4819236326137,78.5411551171801),(17.3227843624735,78.4201698954023),(17.3858281961646,78.4022854930672)]
+        coordinate_list={'Select':'Select', 'Point 1':(17.4990737222447,78.5484425537565),'Point 2':(17.3228369187765,78.4003341997112),'Point 3':(17.4289313805497,78.3074137286198),
+                         'Point 4':(17.5338767879618,78.4312953607305),'Point 5':(17.4501231060621,78.3179062429442),'Point 6':(17.5237690238243,78.5091847750287),
+                         'Point 7':(17.4894454531734,78.4112496777986),'Point 8':(17.4308989846564,78.5238677530526),'Point 9':(17.3011570696757,78.4250332621834),
+                         'Point 10':(17.4819236326137,78.5411551171801)}
+                         #,(17.3227843624735,78.4201698954023),(17.3858281961646,78.4022854930672)]
         pkl_filename = "Est_time_pred_hyd.pkl"
     with open(pkl_filename, 'rb') as file:
         regressor = pickle.load(file)    
